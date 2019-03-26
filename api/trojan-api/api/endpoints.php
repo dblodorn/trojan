7fcdbe9bd27f287ad86515925d2eb49fdf11ead5
dblodorn <dain@db13.us>
2019-03-26 11:05:03 -0700

    added new endpoints

--- api/trojan-api/api/endpoints.php
+++ api/trojan-api/api/endpoints.php
@@ -2,9 +2,9 @@
 
   function main_data(){
     $data = array();
-    $data['options'] = options_data();
+    $data['options'] = get_fields('options');
     $data['artists'] = cpt_artists();
-    // $data['releases'] = cpt_releases();
+    $data['releases'] = cpt_releases();
     return $data;
   }
   
