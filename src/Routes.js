af229fb9620b94694cc9aa81fe7ba217cd488224
dblodorn <dain@db13.us>
2019-04-08 13:16:23 -0700

    removed live

--- src/Routes.js
+++ src/Routes.js
@@ -5,7 +5,6 @@ import NotFound from './views/NotFound'
 import Home from './views/Home'
 import Artists from './views/Artists'
 import Listen from './views/Listen'
-import Live from './views/Live'
 import About from './views/About'
 import Release from './views/Release'
 
@@ -15,7 +14,6 @@ export default () =>
       <Route exact path={'/'} component={props => <Home {...props} />} />
       <Route exact path={'/artists'} component={props => <Artists {...props} />} />
       <Route exact path={'/listen'} component={props => <Listen {...props} />} />
-      <Route exact path={'/live'} component={props => <Live {...props} />} />
       <Route exact path={'/about'} component={props => <About {...props} />} />
       <Route exact path={'/release/:id'} component={props => <Release {...props} />} />
       <Route component={NotFound} />
